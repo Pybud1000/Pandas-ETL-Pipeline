5939362278ba706cfc553518d2edf9e04f44fd3e
Miguel Perlado <perladomiguel7@gmail.com>
2026-08-26 13:10:15 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+orders = pd.read_excel(r'C:\Users\PCXPC\Documents\super secret hehehe\Proj\Pandas ETL\data\raw\orders.xlsx')
+returns = pd.read_excel(r'C:\Users\PCXPC\Documents\super secret hehehe\Proj\Pandas ETL\data\raw\returns.xlsx')
+tickets = pd.read_excel(r'C:\Users\PCXPC\Documents\super secret hehehe\Proj\Pandas ETL\data\raw\tickets.xlsx')
+
+# CLEANING SECTION
+
+# ORDERS
