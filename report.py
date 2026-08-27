e74122a4868a2c117faeb8063614718e87026a46
Miguel Perlado <perladomiguel7@gmail.com>
2026-08-27 17:41:56 +0800

    file setup for reporting layer

---
+++ report.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+orders = pd.read_csv(r'C:\Users\PCXPC\Documents\super secret hehehe\Projects\Pandas ETL\data\clean\clean_orders.csv')
+returns = pd.read_csv(r'C:\Users\PCXPC\Documents\super secret hehehe\Projects\Pandas ETL\data\clean\clean_returns.csv')
+tickets = pd.read_csv(r'C:\Users\PCXPC\Documents\super secret hehehe\Projects\Pandas ETL\data\clean\clean_tickets.csv')
+
